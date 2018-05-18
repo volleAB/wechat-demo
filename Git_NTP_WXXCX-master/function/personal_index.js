4af36d15e50407b45ffed708f6aec29f3387bde9
volleAB <724958674@qq.com>
2018-05-18 22:07:07 +0800

    new

--- Git_NTP_WXXCX-master/function/personal_index.js
+++
@@ -1,9 +0,0 @@
-function set_title(){
-    wx.setNavigationBarTitle({
-        title: 'new民大'
-    });
-}
-
-module.exports = {
-    set_title: set_title,
-}
---
+++ WXXCX_P/function/personal_index.js
@@ -0,0 +1,9 @@
+function set_title(){
+    wx.setNavigationBarTitle({
+        title: 'NEW民大'
+    });
+}
+
+module.exports = {
+    set_title: set_title,
+}
